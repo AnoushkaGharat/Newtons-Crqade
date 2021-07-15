52276cb437690138fb262baf8dc0d620782ac3c3
AnoushkaGharat <83427391+AnoushkaGharat@users.noreply.github.com>
2021-07-15 07:20:31 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,17 +59,18 @@ function draw() {
   bob4.display();
   bob5.display();
   //bob6.display();
-  
-  function keyPressed(){
-	  if(keyCode === UP_ARROW){
-		  Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,
-			            {x:-50,y:-45})
-		  Matter.Body.applyForce(bobObject5.body,bobObject5.body.position,
-						{x:50,y:45})
-	  }
-
-  }
  
+}
+
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(bob1.body,bob1.body.position,
+					  {x:-50,y:-45})
+	   // Matter.Body.applyForce(bobObject5.body,bobObject5.body.position,
+		  //			{x:50,y:45})
+	}
+
 }
 
 
